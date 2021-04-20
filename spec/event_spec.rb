e269617a0df220ced97a5240276541d9c61f0152
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-20 13:31:04 -0600

    Create tests for Event#init

---
+++ spec/event_spec.rb
@@ -0,0 +1,25 @@
+require 'rspec'
+require './lib/food_truck'
+require './lib/item'
+
+describe Event do
+  describe '::new' do
+    it 'exists' do
+      event = Event.new("South Pearl Street Farmers Market")
+
+      expect(event).is_a? Event
+    end
+
+    it 'has a name' do
+      event = Event.new("South Pearl Street Farmers Market")
+
+      expect(event.name).to eq "South Pearl Street Farmers Market"
+    end
+
+    it 'has no food trucks to start' do
+      event = Event.new("South Pearl Street Farmers Market")
+
+      expect(event.food_trucks).to eq []
+    end
+  end
+end
