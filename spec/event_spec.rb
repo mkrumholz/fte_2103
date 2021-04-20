573cc3692cb0794461507723fd9dd12d599dc421
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-20 14:05:07 -0600

    Create test for Event#overstocked_items

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -98,4 +98,99 @@ describe Event do
       expect(event.food_trucks_that_sell(item4)).to eq [food_truck2]
     end
   end
+
+  describe '#sorted_item_list' do
+    xit 'returns of all items FoodTrucks have in stock, sorted' do
+      event = Event.new("South Pearl Street Farmers Market")
+      food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+      food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+      food_truck3 = FoodTruck.new("Palisade Peach Shack")
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+      item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+      item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+      food_truck1.stock(item1, 35)
+      food_truck1.stock(item2, 7)
+      food_truck2.stock(item4, 50)
+      food_truck2.stock(item3, 25)
+      food_truck3.stock(item1, 65)
+      food_truck3.stock(item3, 10)
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+
+
+    end
+  end
+
+  describe '#total_inventory' do
+    it 'returns a hash of all items sold at the event' do
+      #Specifically, it should return a hash with items as keys and hash as values - this sub-hash should have two key/value pairs: quantity pointing to total inventory for that item and food_trucks pointing to an array of the food trucks that sell that item.
+      event = Event.new("South Pearl Street Farmers Market")
+      food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+      food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+      food_truck3 = FoodTruck.new("Palisade Peach Shack")
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+      item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+      item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+      food_truck1.stock(item1, 35)
+      food_truck1.stock(item2, 7)
+      food_truck2.stock(item4, 50)
+      food_truck2.stock(item3, 25)
+      food_truck3.stock(item1, 65)
+      food_truck3.stock(item3, 10)
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+
+      expected = {
+        item1 => {
+          quantity: 100,
+          food_trucks: [food_truck1, food_truck3]
+        },
+        item2 => {
+          quantity: 7,
+          food_trucks: [food_truck1]
+        },
+        item4 => {
+          quantity: 50,
+          food_trucks: [food_truck2]
+        },
+        item3 => {
+          quantity: 35,
+          food_trucks: [food_truck2, food_truck3]
+        },
+      }
+      expect(event.total_inventory).to eq expected
+    end
+  end
+
+  describe '#overstocked_items' do
+    xit 'returns a list of overstocked items' do
+      #An item is overstocked if it is sold by more than 1 food truck AND the total quantity is greater than 50.
+      event = Event.new("South Pearl Street Farmers Market")
+      food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+      food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+      food_truck3 = FoodTruck.new("Palisade Peach Shack")
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+      item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+      item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+      food_truck1.stock(item1, 35)
+      food_truck1.stock(item2, 7)
+      food_truck2.stock(item4, 50)
+      food_truck2.stock(item3, 25)
+      food_truck3.stock(item1, 65)
+      food_truck3.stock(item3, 10)
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+
+
+    end
+  end
 end
